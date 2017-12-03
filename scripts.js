1d2109605982d25ad449274a48270a8cb1dcd70f
Michal <pulka.michal@gmail.com>
2017-12-03 20:17:00 +0100

    Change content

--- scripts.js
+++ scripts.js
@@ -1,11 +1,13 @@
-for(i=1;i<=5;i++){
+for(i = 1; i <= 5; i++){
   var star = "";
   star = "";
-  for(j=i;j<5;j++){
+  for(j = i; j < 5; j++){
     star += " ";
   }
-  for(h=0;h<(i*2)-1;h++){
+  for(h = 0; h < (i * 2) - 1; h++){
     star += "*";
   }
   console.log(star);
 }
+
+
